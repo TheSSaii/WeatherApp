7e1dd5d547ccb161982503263c4b1901c481b233
TheSSaii <morales.ssimon2007@gmail.com>
2026-03-09 10:47:44 -0500

    Agrego función para spinner(), adiciono los resultados de las variables tempMax y tempMin para mostrar temperaturas maxima y minima

--- js/app_weather.js
+++ js/app_weather.js
@@ -20,7 +20,8 @@ function buscarClima(e){
         return;
     }
 
-    const url = (`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=dKEY&units=metric`);
+    const url = (`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=df810338fc132f9e00342f16d38e7dc6&units=metric`);
+    
     consultarAPI(url);
 }
 
@@ -53,19 +54,49 @@ function mostrarError(mensaje){
 function mostrarResultado(data){
     const nombre = data.name;
     const temperatura = data.main.temp;
+    const tempMin = data.main.temp_min;
+    const tempMax = data.main.temp_max;
     const descripcion = data.weather[0].description;
+
 
     resultado.innerHTML = '';
 
+    resultado.classList.add(
+        'bg-green-800',
+        'text-white',
+        'text-center',
+        'p-3',
+        'mt-4',
+        'rounded'
+    );
+
     resultado.innerHTML = `
-    <h2>${nombre}</h2>
-    <p>${temperatura}</p>
-    <p>${descripcion}</p>
+    <h2>nombre de la ciudad: ${nombre}</h2>
+    <p>temperatura minima: ${tempMin}</p>
+    <p>temperatura maxima: ${tempMax}</p>
+    <p>temperatura: ${temperatura}</p>
+    <p>descripción del clima: ${descripcion}</p>
     `;
     
 }
 
+function Spinner(){
+
+    resultado.innerHTML = '';
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('flex','justify-center','mt-6');
+
+    spinner.innerHTML = `
+        <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-yellow-500"></div>
+    `;
+
+    resultado.appendChild(spinner);
+}
+
 async function consultarAPI(url){
+    
+    Spinner();
     try{
         const response = await fetch(url);
 
@@ -80,6 +111,7 @@ async function consultarAPI(url){
             mostrarError("Ciudad no encontrada");
             return;
         }
+
 
         console.log(data);
         mostrarResultado(data);
